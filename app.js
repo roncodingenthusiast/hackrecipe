6c8cca6613082e3017bce60e17b78327114f90d2
Ronald Ekambi <ronekambi@gmail.com>
2016-10-03 08:55:40 -0400

    got SPA and routing working perfectly

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
-angular.module('angularMainApp', [])
-.controller('listOfRecipes', ['$scope', function($scope){}]);
+var app = angular.module('angularMainApp', ['ngRoute']);
+
+app.controller('listOfRecipes', ['$scope', function($scope){}]);
+
+app.config(function($routeProvider){
+	$routeProvider.when('/', {
+		templateUrl: 'templates/home.html',
+		controller: 'listOfRecipes'
+	})
+});
