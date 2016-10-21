035202396d539b104ce37ec4002fedb64ccdc467
Ekambi Ronald <Ekambi Ronald>
2016-10-21 14:57:38 -0400

    added comments

--- controllers/config.js
+++ controllers/config.js
@@ -1,29 +1,35 @@
+/**
+	Author: Ronald Ekambi 
+	Purpose of file: provides routes for application. 
+	Define which file should be loaded depeding on URL 
+**/
 app.config(function($routeProvider){
 	$routeProvider.when('/', {
-		templateUrl: 'resources/templates/home.html',
+		templateUrl: 'templates/home.html',
 		controller: 'mainCtrl'
 	})
 	.when('/listQuestions',{
-		templateUrl: 'resources/templates/questionslist.html', 
+		templateUrl: 'templates/questionslist.html', 
 		controller: 'listOfQuestionsCtrl'
 
 	}).when('/questionsDetails/:questionid', {
-		templateUrl: 'resources/templates/questionsdetails.html',
+		templateUrl: 'templates/questionsdetails.html',
 		controller: 'questionsDetailsCtrl'
 	})
 	.when('/login', {
-		templateUrl: 'resources/templates/login.html', 
+		templateUrl: 'templates/login.html', 
 		controller: 'loginCtrl'
 	})
 	.when('/register', {
-		templateUrl: 'resources/templates/register.html', 
+		templateUrl: 'templates/register.html', 
 		controller: 'registerCtrl'
 	})
 	.when('/quiz',{
-		templateUrl: 'resources/templates/quiz.html',
+		templateUrl: 'templates/quiz.html',
 		controller: 'quizctrl'
 	})
+	//if the route is not recognised then it redirects you to home page
 	.otherwise({
-		redirecTo: '/listQuestions'
+		redirecTo: '/'
 	});
 });
