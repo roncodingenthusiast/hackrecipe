e71cff4024c0b671e797771f06412ecdf1122caa
Ekambi Ronald <Ekambi Ronald>
2016-10-21 11:39:40 -0400

    seperated controllers and config

--- app.js
+++ app.js
@@ -1,10 +1,1 @@
 var app = angular.module('angularMainApp', ['ngRoute']);
-
-app.controller('listOfRecipes', ['$scope', function($scope){}]);
-
-app.config(function($routeProvider){
-	$routeProvider.when('/', {
-		templateUrl: 'templates/home.html',
-		controller: 'listOfRecipes'
-	})
-});
---
+++ controllers/config.js
@@ -0,0 +1,6 @@
+app.config(function($routeProvider){
+	$routeProvider.when('/', {
+		templateUrl: 'templates/home.html',
+		controller: 'listOfRecipes'
+	})
+});
