398671ebf471547506427d781d639dc53a5bd4ea
Ekambi Ronald <Ekambi Ronald>
2016-10-30 07:49:01 -0400

    added git ignore and initialised files for testing

--- controllers/maincontroller.js
+++ controllers/maincontroller.js
@@ -1,10 +1,11 @@
 /**
 	Author: Ronald Ekambi 
-	Purpose of file: 
+	Purpose of file: just test data.json
 **/
 app.controller('mainCtrl', ['$scope', '$http', function($scope, $http){
 	
-	$http.get('recipedata.php').then(function(recipedata){
-		$scope.list = recipedata.data;		
+	$http.get('data.json').then(function(recipedata){
+		$scope.list = recipedata.data;
+		console.log($scope.list);		
 	});
 }])
